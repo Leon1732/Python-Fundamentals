2206bb456b6a2304fa6407e86b9f3e2a971a507a
Leon1732 <134924773+Leon1732@users.noreply.github.com>
2023-12-15 22:24:25 +0800

    Delete Object-Oriented_Programming/Module/11-2 directory

--- Object-Oriented_Programming/Module/11-2/but.py
+++
@@ -1,4 +0,0 @@
-import module_test
-
-#module_test.m_t_pr()
-#print('使用外部模块“module_test”中的变量：',module_test.name)
--- Object-Oriented_Programming/Module/11-2/module_test.py
+++
@@ -1,4 +0,0 @@
-print('导入的测试模块的输出')
-name = 'module_test'
-def m_t_pr():
-    print('模块module_test中m_t_pr()函数')
