f4f4973cbd4b39fd2dc4d7cacdfaa8ce0fae7ce0
Leon1732 <134924773+Leon1732@users.noreply.github.com>
2023-12-15 23:51:53 +0800

    Add files via upload

---
+++ Object-Oriented_Programming/Package/11-5/bao.py
@@ -0,0 +1,6 @@
+import pckage
+import pckage.tt
+print("输出包pckage中的变量name:",pckage.name)
+print('调用包pckage中的函数：',end='')
+pckage.pck_test_fun()
+pckage.tt.tt()
---
+++ Object-Oriented_Programming/Package/11-5/pckage/__init__.py
@@ -0,0 +1,5 @@
+name = 'pckage'
+print('__init__.py中输出:',name)
+
+def pck_test_fun():
+    print('包pckage中的方法pck_test_fun')
